bde115560c79d05dae8b8205c6c25f66d14c90e9
genioso <dwright@bronxnet.org>
2019-05-20 17:16:49 -0400

    created man and dog app

---
+++ mandog.py
@@ -0,0 +1,14 @@
+class Man (first_name, last_name, feed_action):
+    name = first_name + last_name
+
+    def feed (feed_action):
+        if feed_action ():
+            return "man just fed dog"
+        else:
+            return "man did not feed dog"
+
+class Dog (name, bark):
+    return name
+
+    def bark ():
+        return "Woof!"
